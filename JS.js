6fb327831140d6ed02b701498f76a1cc4c57b4ac
CARR0T02 <mr.gabriel.chang@gmail.com>
2023-03-15 15:03:30 +0800

    Add ability to use math functions and clear feature

--- JS.js
+++ JS.js
@@ -1,21 +1,57 @@
-let currNum = "0";
-let newNum = "0";
-let display = "";
+var firstNum = 0;
+var secondNum = 0;
+let display = "0";
+let currOperator = "";
+
+const calculator = document.querySelector("#calculator");
+calculator.addEventListener("click", click);
 
 function click(e) {
   if (e.target.nodeName !== "BUTTON") {
     return;
   }
-  if (e.target.value != "") {
-    updateNumber();
+  if (!isEmpty(e.target.value)) {
+    appendDisplay(e.target.value);
+  } else if (!isEmpty(e.target.dataset.operator)) {
+    if (isEmpty(currOperator)) {
+      currOperator = e.target.dataset.operator;
+      updateNumber("firstNum");
+      display = "";
+    } else {
+      handleEquals();
+      currOperator = e.target.dataset.operator;
+    }
+  } else if (e.target.dataset.misc == "=") {
+    handleEquals();
   } else {
-    console.log(e.target.dataset.operator);
+    updateNumber("firstNum");
+    misc(e.target.dataset.misc);
   }
 }
 
-function updateDisplay(display) {
+function appendDisplay(userInput) {
+  if (!Number(display)) {
+    display = userInput;
+  } else if (display.length < 13) {
+    display += userInput;
+  }
+  updateDisplay(display);
+}
+
+function updateDisplay(input = display) {
   const displayBar = document.querySelector("#displayBar");
-  displayBar.textContent = display;
+  displayBar.textContent = input;
+}
+
+function updateNumber(number) {
+  window[number] = Number(display);
+}
+
+function handleEquals() {
+  updateNumber("secondNum");
+  firstNum = operate(currOperator, firstNum, secondNum);
+  display = "";
+  updateDisplay(firstNum);
 }
 
 function add(a, b) {
@@ -34,5 +70,36 @@ function divide(a, b) {
   return a / b;
 }
 
-const calculator = document.querySelector("#calculator");
-calculator.addEventListener("click", click);
+function isEmpty(input) {
+  if (input) return false;
+  else return true;
+}
+
+function operate(operator, a, b) {
+  switch (operator) {
+    case "/":
+      return divide(a, b);
+    case "x":
+      return multiply(a, b);
+    case "+":
+      return add(a, b);
+    case "-":
+      return subtract(a, b);
+  }
+}
+
+function misc(misc) {
+  switch (misc) {
+    case "C":
+      firstNum = "0";
+      display = "0";
+      currOperator = "";
+      updateDisplay();
+      break;
+    // Negative button does not work
+    case "--":
+      firstNum *= -1;
+      display = firstNum.toString();
+      return display;
+  }
+}
